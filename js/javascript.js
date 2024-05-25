4c262f0152e0fe707f3dd72c416da5ea82219f47
Ahmed Reda <156605444+ahmedredall@users.noreply.github.com>
2024-05-25 20:12:34 +0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,7 @@
+function test(){alert("The text has been copied");}
+
+
+// Initialize tooltips
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)})
